574878273eaecc536d3e95dd6dcbe19ade154764
Denis Makarchuk <hiedmant@gmail.com>
2019-09-27 16:09:50 +0300

    [+] kata_2 & kata_2_optima

---
+++ Katas/Kata_2_Optima/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_2_Optima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsIsogram("Dermatoglyphics"));
+            Console.WriteLine(IsIsogram("moose"));
+            Console.WriteLine(IsIsogram("aba"));
+
+            Console.Read();
+        }
+
+        public static bool IsIsogram(string str)
+        {
+            return str.ToUpper().Distinct().Count() == str.Length;
+        }
+    }
+}
