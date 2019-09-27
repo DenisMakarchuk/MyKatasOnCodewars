820d5ae68252c6a106132f4a8844cc5ed1b3d3dd
Denis Makarchuk <hiedmant@gmail.com>
2019-09-27 16:29:40 +0300

    [+] kata_3 & kata_3_optima

---
+++ Katas/Kata_3_Optima/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_3_Optima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 0, 0, 1 }));
+            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 0, 1, 0 }));
+            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 1, 0, 1 }));
+            Console.WriteLine(binaryArrayToNumber(new int[] { 1, 0, 0, 1 }));
+            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 1, 1, 0 }));
+            Console.WriteLine(binaryArrayToNumber(new int[] { 1, 1, 1, 1 }));
+            Console.WriteLine(binaryArrayToNumber(new int[] { 1, 0, 1, 1 }));
+
+            Console.Read();
+        }
+
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            return Convert.ToInt32(string.Join("", BinaryArray), 2);
+        }
+    }
+}
