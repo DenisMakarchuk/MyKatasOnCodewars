fe4ca73c57690626a61a3246c92804977892fb74
Denis Makarchuk <hiedmant@gmail.com>
2019-09-28 15:36:35 +0300

    some changed [+] kata_5 & kata_5_optima

--- Katas/Kata_3/Program.cs
+++ Katas/Kata_3/Program.cs
@@ -10,25 +10,25 @@ namespace Kata_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 0, 0, 1 }));
-            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 0, 1, 0 }));
-            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 1, 0, 1 }));
-            Console.WriteLine(binaryArrayToNumber(new int[] { 1, 0, 0, 1 }));
-            Console.WriteLine(binaryArrayToNumber(new int[] { 0, 1, 1, 0 }));
-            Console.WriteLine(binaryArrayToNumber(new int[] { 1, 1, 1, 1 }));
-            Console.WriteLine(binaryArrayToNumber(new int[] { 1, 0, 1, 1 }));
+            Console.WriteLine(BinaryArrayToNumber(new int[] { 0, 0, 0, 1 }));
+            Console.WriteLine(BinaryArrayToNumber(new int[] { 0, 0, 1, 0 }));
+            Console.WriteLine(BinaryArrayToNumber(new int[] { 0, 1, 0, 1 }));
+            Console.WriteLine(BinaryArrayToNumber(new int[] { 1, 0, 0, 1 }));
+            Console.WriteLine(BinaryArrayToNumber(new int[] { 0, 1, 1, 0 }));
+            Console.WriteLine(BinaryArrayToNumber(new int[] { 1, 1, 1, 1 }));
+            Console.WriteLine(BinaryArrayToNumber(new int[] { 1, 0, 1, 1 }));
 
             Console.Read();
         }
 
-        public static int binaryArrayToNumber(int[] BinaryArray)
+        public static int BinaryArrayToNumber(int[] binaryArray)
         {
             int degree = 0;
             int num = 0;
 
-            for (int i = BinaryArray.Length - 1; i >= 0; i--)
+            for (int i = binaryArray.Length - 1; i >= 0; i--)
             {
-                if (BinaryArray[i] != 0)
+                if (binaryArray[i] != 0)
                 {
                     num += (int)Math.Pow(2, degree);
                 }
