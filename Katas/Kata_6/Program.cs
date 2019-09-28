b24682be0bc737cbea12453ad6fd2fd3ace4d454
Denis Makarchuk <hiedmant@gmail.com>
2019-09-28 19:58:48 +0300

    [+] kata_6

---
+++ Katas/Kata_6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindShort("1 Sdad afa asdd df edfes"));
+
+            Console.Read();
+        }
+
+        public static int FindShort(string s)
+        {
+            int num = s.Length, tempNum = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+
+                if (char.IsDigit(s[i]) || char.IsLetter(s[i]))
+                {
+                    tempNum++;
+                }
+                else
+                {
+                    if (tempNum < num && tempNum != 0)
+                    {
+                        num = tempNum;
+                    }
+                    tempNum = 0;
+                }
+            }
+
+            if (tempNum < num)
+            {
+                num = tempNum;
+            }
+
+            return num;
+        }
+    }
+}
