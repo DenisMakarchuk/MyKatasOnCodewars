d83bd5bb3e870b9479b85a864f385c43acd40a03
Denis Makarchuk <hiedmant@gmail.com>
2019-09-29 16:01:54 +0300

    [+] kata_8 & kata_8_optima

---
+++ Katas/Kata_8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToJadenCase("How can mirrors be real if our eyes aren't real"));
+
+            Console.Read();
+        }
+
+        public static string ToJadenCase(string phrase)
+        {
+            return string.Join(" ", phrase.Split(' ').Select((x) => char.ToUpper(x[0]) + x.Remove(0, 1)));
+        }
+    }
+}
