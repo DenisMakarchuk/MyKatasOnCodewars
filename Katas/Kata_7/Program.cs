0d1495bb7b09fe5423a0ba6be37405a59421e314
Denis Makarchuk <hiedmant@gmail.com>
2019-09-29 14:16:30 +0300

    [+] kata_7 & kata_7_optima

---
+++ Katas/Kata_7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ExpandedForm(12));
+            Console.WriteLine(ExpandedForm(42));
+            Console.WriteLine(ExpandedForm(703040));
+
+            Console.Read();
+        }
+
+        public static string ExpandedForm(long num)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string str = Convert.ToString(num);
+            int counterOfZero = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!string.Equals(str[i],'0'))
+                {
+                    if (i > 0)
+                    {
+                        stringBuilder.Append(" + ");
+                    }
+
+                    counterOfZero = str.Length - i - 1;
+                    stringBuilder.Append(str[i]);
+
+                    while (counterOfZero > 0)
+                    { 
+                        stringBuilder.Append("0");
+                        counterOfZero--;
+                    }
+                }
+            }
+
+            return str = Convert.ToString(stringBuilder);
+        }
+    }
+}
---
+++ Katas/Kata_7_Optima/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_7_Optima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ExpandedForm(12));
+            Console.WriteLine(ExpandedForm(42));
+            Console.WriteLine(ExpandedForm(703040));
+
+            Console.Read();
+        }
+
+        public static string ExpandedForm(long num)
+        {
+            var str = num.ToString();
+
+            return String.Join(" + ", str
+                .Select((x, i) => char.GetNumericValue(x) * Math.Pow(10, str.Length - i - 1))
+                .Where(x => x > 0));
+        }
+    }
+}
