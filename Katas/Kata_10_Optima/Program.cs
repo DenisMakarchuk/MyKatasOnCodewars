3b70164cc95be2a504ba7f185a6a87dce59f2e6b
Denis Makarchuk <hiedmant@gmail.com>
2019-10-05 20:25:09 +0300

    [+] kata_10 & kata_10_optima

---
+++ Katas/Kata_10_Optima/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_10_Optima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = Wave("two words");
+
+            foreach (string item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+
+        public static List<string> Wave(string str) =>  
+            str.Select((c, i) => str.Substring(0, i) + Char.ToUpper(c) + str.Substring(i + 1))
+            .Where(x => x != str)
+            .ToList();
+    }
+}
