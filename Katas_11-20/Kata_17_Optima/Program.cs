1448dfcfe21f759e1910954b06166651ae211c95
Denis Makarchuk <hiedmant@gmail.com>
2019-10-24 19:17:43 +0300

    [+] kata_17 @ kata_17_optima

---
+++ Katas_11-20/Kata_17_Optima/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_17_Optima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in Beggars(new int[] { 1, 2, 3, 4, 5 }, 0))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+
+        public static int[] Beggars(int[] values, int n)
+        {
+            int[] beggars = new int[n];
+
+            if (n == 0)
+            {
+                return beggars;
+            }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                beggars[i % n] += values[i];
+            }
+
+            return beggars;
+        }
+    }
+}
